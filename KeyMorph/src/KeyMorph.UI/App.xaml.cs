ffeecd919a6defaab79003f918af1003288102bc
parham <parhamghahremani1400@gmail.com>
2026-07-14 22:45:11 +0330

    refactor: reorganize solution structure

---
+++ KeyMorph/src/KeyMorph.UI/App.xaml.cs
@@ -0,0 +1,12 @@
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace KeyMorph.UI {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application // <--- تغییر این خط
+    {
+    }
+}
